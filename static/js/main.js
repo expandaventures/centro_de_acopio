71ee142c665b1df82f7f569724ef29ae86674e32
Jose Juan Garcia Rojas <juan@sintrafico.com>
2017-09-22 17:46:11 -0500

    Add invoice view

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,7 @@ function get_data(url, data, cb) {
   });
 }
 
-function post_data(url, data, cb_success, cb_error) {
+function post_data(url, data, cb_success, cb_error, type) {
   $.ajax({
     crossDomain:true,
     headers: {'X-Requested-With': apiKey,},
@@ -25,7 +25,7 @@ function post_data(url, data, cb_success, cb_error) {
     dataType: 'json',
     async: true,
     success:function(e) {
-      (cb_success)(e);
+      (cb_success)(e, type);
     },
     error: function(e) {
       (cb_error)(e)
@@ -33,8 +33,7 @@ function post_data(url, data, cb_success, cb_error) {
   });
 }
 
-function put_data(url, data, cb_success, cb_error) {
-  data.apiKey = apiKey;
+function put_data(url, data, cb_success, cb_error, type) {
   $.ajax({
     crossDomain:true,
     headers: {'X-Requested-With': apiKey},
@@ -44,7 +43,7 @@ function put_data(url, data, cb_success, cb_error) {
     dataType: 'json',
     async: true,
     success:function(e) {
-      (cb_success)(e);
+      (cb_success)(e, type);
     },
     error: function(e) {
       (cb_error)(e)
@@ -52,7 +51,7 @@ function put_data(url, data, cb_success, cb_error) {
   });
 }
 
-function delete_data(url, data, cb_success, cb_error) {
+function delete_data(url, data, cb_success, cb_error, type) {
   $.ajax({
     crossDomain:true,
     headers: {'X-Requested-With': apiKey,},
@@ -62,7 +61,7 @@ function delete_data(url, data, cb_success, cb_error) {
     dataType: 'json',
     async: true,
     success:function(e) {
-      (cb_success)(e);
+      (cb_success)(e, type);
     },
     error: function(e) {
       (cb_error)(e)
@@ -109,12 +108,65 @@ function inventory_table(raw_data) {
   }
 }
 
+function invoice_table(raw_data) {
+  let tbody = document.getElementById('inventory-info');
+  $('#modifyInvoice').on('show.bs.modal', function (e) {
+    let button = $(e.relatedTarget);
+    let item_id = button.data('id');
+    document.getElementById('item-id-modify').value = item_id;
+    let row_data = document.getElementById(item_id).cells;
+    document.getElementById('current-location').value = row_data[0].innerHTML;
+    document.getElementById('current-product').value = row_data[1].innerHTML;
+    document.getElementById('current-quantity').value = row_data[2].innerHTML;
+  });
+  if (raw_data.status = 'OK') {
+    tbody.innerHTML = '';
+    let data = raw_data.invoice;
+    for (let i = 0; i < data.length; i++) {
+      let tr = document.createElement('tr');
+      let item_id = data[i].invoice;
+      tr.id = item_id;
+      let verified = data[i].verified
+            ?'<span class="glyphicon glyphicon-ok-circle v-true" ></span>'
+            :'<span class="glyphicon glyphicon-remove-circle v-false" ></span>'
+
+      tr.innerHTML = '<td>' + data[i].location + '</td>'
+                    + '<td>' + data[i].product + '</td>'
+                    + '<td>' + data[i].quantity + '</td>'
+                    + '<td>' + verified + '</td>'
+                    + '<td>' + data[i].warehouse + '</td>'
+                    + '<td>'
+                      + '<button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#modifyInvoice" data-id="'+item_id+'">'
+                        + '<span class="glyphicon glyphicon-pencil" ></span>'
+                      +'</button>'
+                    + '</td>'
+                    + '<td>'
+                      + '<button type="button" class="btn btn-danger btn-xs" id="del-'+item_id+'">'
+                        +'<span class="glyphicon glyphicon-remove" ></span>'
+                      +'</button>'
+                    + '</td>'
+      tbody.appendChild(tr);
+      document.getElementById('del-'+item_id).addEventListener('click', function(e){
+        delete_data('https://api.sintrafico.com/invoice/' + item_id, {}, post_success, post_fail, 'invoice')
+      });
+    }
+  }
+}
+
 function saveProduct() {
   let location = document.getElementById('location').value,
       product = document.getElementById('product').value,
       quantity = document.getElementById('quantity').value;
   let data = {'product': product, 'location': location, 'quantity': parseInt(quantity)}
   post_data('https://api.sintrafico.com/inventory', data, post_success, post_fail)
+}
+
+function saveInvoice() {
+  let location = document.getElementById('location').value,
+      product = document.getElementById('product').value,
+      quantity = document.getElementById('quantity').value;
+  let data = {'product': product, 'location': location, 'quantity': parseInt(quantity), 'warehouse': 'No asignada'}
+  post_data('https://api.sintrafico.com/invoice', data, post_success, post_fail, 'invoice' )
 }
 
 function updateProduct() {
@@ -126,11 +178,36 @@ function updateProduct() {
   put_data('https://api.sintrafico.com/inventory/'+item_id, data, post_success, post_fail)
 }
 
-function post_success(e){
-  $('#newProduct').modal('hide')
-  $('#modifyProduct').modal('hide')
+function updateInvoices() {
+  let location = document.getElementById('current-location').value,
+      product = document.getElementById('current-product').value,
+      quantity = document.getElementById('current-quantity').value,
+      verified = document.getElementById('current-verified').checked,
+      warehouse = document.getElementById('current-warehouse').value,
+      item_id = document.getElementById('item-id-modify').value;
+  let data = {'product': product, 'location': location, 'quantity': parseInt(quantity), 'warehouse': warehouse}
+  if (verified) {
+    data.verified = true;
+  }
+  put_data('https://api.sintrafico.com/invoice/'+item_id, data, post_success, post_fail, 'invoice')
+}
+
+function post_success(e, type){
+  let url = '', cb;
+  if (type != 'invoice') {
+    $('#newProduct').modal('hide')
+    $('#modifyProduct').modal('hide')
+    url = 'https://api.sintrafico.com/inventory';
+    cb = inventory_table;
+  } else {
+    $('#newInvoice').modal('hide')
+    $('#modifyInvoice').modal('hide')
+    url = 'https://api.sintrafico.com/invoice';
+    cb = invoice_table;
+  }
+  get_data(url, {}, cb)
+
   let success = document.getElementById('ok-post');
-  get_data('https://api.sintrafico.com/inventory', {}, inventory_table)
   success.classList.toggle("hidden");
   setTimeout(function(){ success.classList.toggle("hidden"); }, 2000);
 }
